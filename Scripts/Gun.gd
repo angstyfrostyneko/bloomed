5ea31bba9ca6d813c4a46f58bc0b16b0552a20ac
RubberDuckShobe <dog@dog.dog>
2022-12-27 15:04:00 +0100

    Basic bullet particles

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -6,6 +6,7 @@ var weapon_class = Weapon.Class.RIFLE
 # For the player to access more easily
 onready var cooldown = $Cooldown
 onready var reload_timer = $Reload
+onready var bullet_particles = $BulletParticles
 
 var damage = 10
 var magazine = 100
